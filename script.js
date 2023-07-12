7594ff69b777e578ad903f15185101ac140ad453
Staroyee <danmasefield@gmail.com>
2023-07-12 22:19:54 +0930

    Added test function called 'time' to script.js to add and remove the classes defining past, future, and present.

--- script.js
+++ script.js
@@ -22,26 +22,7 @@ $(function () {
     console.log(hours);
   });
 
-  function loadStorage() {
-    const desc9 = localStorage.getItem('hour-9');
-    document.querySelector('.description9').value = desc9;
-    const desc10 = localStorage.getItem('hour-10');
-    document.querySelector('.description10').value = desc10;
-    const desc11 = localStorage.getItem('hour-11');
-    document.querySelector('.description11').value = desc11;
-    const desc12 = localStorage.getItem('hour-12');
-    document.querySelector('.description12').value = desc12;
-    const desc1 = localStorage.getItem('hour-1');
-    document.querySelector('.description1').value = desc1;
-    const desc2 = localStorage.getItem('hour-2');
-    document.querySelector('.description2').value = desc2;
-    const desc3 = localStorage.getItem('hour-3');
-    document.querySelector('.description3').value = desc3;
-    const desc4 = localStorage.getItem('hour-4');
-    document.querySelector('.description4').value = desc4;
-    const desc5 = localStorage.getItem('hour-5');
-    document.querySelector('.description5').value = desc5;
-  }
+  
 
   loadStorage();
 
@@ -55,16 +36,42 @@ $(function () {
 // hour9.setAttribute("class", "row time-block past");
 // hour9.setAttribute("class", "row time-block present");
 // hour9.setAttribute("class", "row time-block future");
-
+function time() {
 var currentTime = dayjs().format('H');
-console.log(currentTime);
+console.log(currentTime)
+if (currentTime == 9) {
+  $("#hour-9").removeClass("past");
+  $("#hour-9").removeClass("future");
+  $("#hour-9").addClass("present");
+} else if (currentTime < 9) {
+  $("#hour-9").removeClass("present");
+  $("#hour-9").removeClass("past");
+  $("#hour-9").addClass("future");
+} else if (currentTime > 9) {
+  $("#hour-9").removeClass("present");
+  $("#hour-9").removeClass("future");
+  $("#hour-9").addClass("past");
+}
+}
+
+time()
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
 
-
+function loadStorage() {
+  $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+  $("#hour-10 .description").val(localStorage.getItem("hour-10"));
+  $("#hour-11 .description").val(localStorage.getItem("hour-11"));
+  $("#hour-12 .description").val(localStorage.getItem("hour-12"));
+  $("#hour-1 .description").val(localStorage.getItem("hour-1"));
+  $("#hour-2 .description").val(localStorage.getItem("hour-2"));
+  $("#hour-3 .description").val(localStorage.getItem("hour-3"));
+  $("#hour-4 .description").val(localStorage.getItem("hour-4"));
+  $("#hour-5 .description").val(localStorage.getItem("hour-5"));
+  }
 
   // TODO: Add code to display the current date in the header of the page.
 
