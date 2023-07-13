07b2ba942f76945668a290119b0b2fe064b198c9
Staroyee <danmasefield@gmail.com>
2023-07-13 13:29:27 +0930

    Added code to complete the 'pastPresentFuture' function in script.js file

--- script.js
+++ script.js
@@ -36,7 +36,7 @@ $(function () {
 // hour9.setAttribute("class", "row time-block past");
 // hour9.setAttribute("class", "row time-block present");
 // hour9.setAttribute("class", "row time-block future");
-function time() {
+function pastPresentFuture() {
 var currentTime = dayjs().format('H');
 console.log(currentTime)
 if (currentTime == 9) {
@@ -52,9 +52,121 @@ if (currentTime == 9) {
   $("#hour-9").removeClass("future");
   $("#hour-9").addClass("past");
 }
+
+if (currentTime == 10) {
+  $("#hour-10").removeClass("past");
+  $("#hour-10").removeClass("future");
+  $("#hour-10").addClass("present");
+} else if (currentTime < 10) {
+  $("#hour-10").removeClass("present");
+  $("#hour-10").removeClass("past");
+  $("#hour-10").addClass("future");
+} else if (currentTime > 10) {
+  $("#hour-10").removeClass("present");
+  $("#hour-10").removeClass("future");
+  $("#hour-10").addClass("past");
 }
 
-time()
+if (currentTime == 11) {
+  $("#hour-11").removeClass("past");
+  $("#hour-11").removeClass("future");
+  $("#hour-11").addClass("present");
+} else if (currentTime < 11) {
+  $("#hour-11").removeClass("present");
+  $("#hour-11").removeClass("past");
+  $("#hour-11").addClass("future");
+} else if (currentTime > 11) {
+  $("#hour-11").removeClass("present");
+  $("#hour-11").removeClass("future");
+  $("#hour-11").addClass("past");
+}
+
+if (currentTime == 12) {
+  $("#hour-12").removeClass("past");
+  $("#hour-12").removeClass("future");
+  $("#hour-12").addClass("present");
+} else if (currentTime < 12) {
+  $("#hour-12").removeClass("present");
+  $("#hour-12").removeClass("past");
+  $("#hour-12").addClass("future");
+} else if (currentTime > 12) {
+  $("#hour-12").removeClass("present");
+  $("#hour-12").removeClass("future");
+  $("#hour-12").addClass("past");
+}
+
+if (currentTime == 13) {
+  $("#hour-1").removeClass("past");
+  $("#hour-1").removeClass("future");
+  $("#hour-1").addClass("present");
+} else if (currentTime < 13) {
+  $("#hour-1").removeClass("present");
+  $("#hour-1").removeClass("past");
+  $("#hour-1").addClass("future");
+} else if (currentTime > 13) {
+  $("#hour-1").removeClass("present");
+  $("#hour-1").removeClass("future");
+  $("#hour-1").addClass("past");
+}
+
+if (currentTime == 14) {
+  $("#hour-2").removeClass("past");
+  $("#hour-2").removeClass("future");
+  $("#hour-2").addClass("present");
+} else if (currentTime < 14) {
+  $("#hour-2").removeClass("present");
+  $("#hour-2").removeClass("past");
+  $("#hour-2").addClass("future");
+} else if (currentTime > 14) {
+  $("#hour-2").removeClass("present");
+  $("#hour-2").removeClass("future");
+  $("#hour-2").addClass("past");
+}
+
+if (currentTime == 15) {
+  $("#hour-3").removeClass("past");
+  $("#hour-3").removeClass("future");
+  $("#hour-3").addClass("present");
+} else if (currentTime < 15) {
+  $("#hour-3").removeClass("present");
+  $("#hour-3").removeClass("past");
+  $("#hour-3").addClass("future");
+} else if (currentTime > 15) {
+  $("#hour-3").removeClass("present");
+  $("#hour-3").removeClass("future");
+  $("#hour-3").addClass("past");
+}
+
+if (currentTime == 16) {
+  $("#hour-4").removeClass("past");
+  $("#hour-4").removeClass("future");
+  $("#hour-4").addClass("present");
+} else if (currentTime < 16) {
+  $("#hour-4").removeClass("present");
+  $("#hour-4").removeClass("past");
+  $("#hour-4").addClass("future");
+} else if (currentTime > 16) {
+  $("#hour-4").removeClass("present");
+  $("#hour-4").removeClass("future");
+  $("#hour-4").addClass("past");
+}
+
+if (currentTime == 17) {
+  $("#hour-5").removeClass("past");
+  $("#hour-5").removeClass("future");
+  $("#hour-5").addClass("present");
+} else if (currentTime < 17) {
+  $("#hour-5").removeClass("present");
+  $("#hour-5").removeClass("past");
+  $("#hour-5").addClass("future");
+} else if (currentTime > 17) {
+  $("#hour-5").removeClass("present");
+  $("#hour-5").removeClass("future");
+  $("#hour-5").addClass("past");
+}
+}
+
+pastPresentFuture()
 
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
